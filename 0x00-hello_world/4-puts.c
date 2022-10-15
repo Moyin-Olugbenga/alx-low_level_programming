d7b6d68e00d876138c2266a1f65b1ada24b84a8e
Moyin-Olugbenga <olugbengamoyinoluwa839@gmail.com>
2022-10-15 09:29:25 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/*
- * main - prints a line of code using puts
- *
- * Return: 0
-*/
-int main(void)
+/*                                                                                                                                                      * main - print a line of code using puts           
+ *                                                                                                                                                      * return: 0
+ */ 
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
